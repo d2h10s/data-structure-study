e517a31ffe1e036257ea9635a2f3de39f36ac5f9
d2_h10s <ecanus9@gmail.com>
2020-02-15 14:43:05 +0900

    Que.h

---
+++ Tree/Tree/Que.h
@@ -0,0 +1,107 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Que {
+public:
+	void enqueue(int);
+	int dequeue();
+	void disp();
+	~Que();
+private:
+	class node {
+	public:
+		int data;
+		node* next;
+		node* prev;
+		node(int n) : data(n) {
+			prev = next = nullptr;
+		};
+	};
+	node* head = nullptr;
+	node* tail = nullptr;
+	node* cur = nullptr; // current
+	node* pre = nullptr; // prev
+	int ncnt = 0; // node counter
+};
+
+void Que::enqueue(int n) {
+	if (!ncnt) {
+		head = new node(n);
+		tail = head;
+	}
+	else if (ncnt == 1) {
+		tail = new node(n);
+		head->next = tail;
+		tail->prev = head;
+	}
+	else {
+		cur = tail;
+		tail = new node(n);
+		cur->next = tail;
+		tail->prev = cur;
+	}
+	ncnt++;
+}
+int Que::dequeue() {
+	if (ncnt < 1) {
+		cout << "Que is empty";
+		return -1;
+	}
+	int tmp = head->data;
+	if (ncnt == 1) {
+		delete head;
+	}
+	else if (ncnt == 2) {
+		delete head;
+		head = tail;
+	}
+	else {
+		cur = head->next;
+		delete head;
+		head = cur;
+	}
+	ncnt--;
+	return tmp;
+}
+
+void Que::disp() {
+	string msg;
+	if (ncnt < 1) {
+		msg = "data does not exist\n\n";
+	}
+	else {
+		msg = "list: [ ";
+		if (head == tail) {
+			msg += to_string(head->data);
+		}
+
+		else {
+			cur = head;
+			for (int i = 0; cur; i++) {
+				msg += to_string(cur->data);
+				if (i < ncnt - 1) msg += ", ";
+				cur = cur->next;
+			}
+		}
+		msg += " ]\n\n";
+	}
+	cout << msg;
+}
+
+Que::~Que() {
+	if (ncnt == 1) {
+		delete head;
+		return;
+	}
+	for (int i = 0; i < ncnt; i++) {
+		if (!head->next) {
+			delete head;
+			return;
+		}
+		cur = head->next;
+		delete head;
+		head = cur;
+	}
+}
